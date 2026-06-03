8bccdd3f69d8d5ff709745c5e9f5c63232239cf5
VacantThinker <littlehappyforsmile@outlook.com>
2026-06-04 06:40:25 +0800

    chore: bump version to 2026.0604.0640 and publish

--- src/DomainORM.js
+++ src/DomainORM.js
@@ -2,8 +2,11 @@ import {BaseORM} from './BaseORM.js';
 
 export class DomainORM extends BaseORM {
   constructor(domain) {
-    super(``, domain, {
+    super(`domainKey`, domain, {
 
     });
+  }
+  logKey(){
+    console.info(this.id); // domainKey 123sdfsdf
   }
 }
