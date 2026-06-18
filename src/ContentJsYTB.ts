e5123d783e379b62d6673945235b9be91d0052a5
VacantThinker <littlehappyforsmile@outlook.com>
2026-06-18 12:31:58 +0800

    chore: bump version to 2026.0618.1231 and push to CI

--- src/ContentJsYTB.ts
+++ src/ContentJsYTB.ts
@@ -1,7 +1,7 @@
 import {tabOpCreateActiveTrue, tabOpCreateKeepOnlyOneFocusTrue} from "./opTab";
 import {browserBrowsingDataRemoveDomainCache} from "./browserBrowsingData";
 import {browserRuntimeSendMessage} from "./browserRuntime";
-import {MessagePayloadAct, MessagePayloadFocusTargetTab} from "./types";
+import {MessagePayloadAct, MessagePayloadOperationTargetTab} from "./types";
 
 /**
  * Captures an <img> element from the current document, draws it to a canvas
@@ -124,7 +124,7 @@ export async function ctJsCloseTab() {
 }
 
 export async function ctJsFocusTargetTab(targetTabId: number) {
-  const message: MessagePayloadFocusTargetTab = {
+  const message: MessagePayloadOperationTargetTab = {
     targetTabId,
     act: 'actFocusTargetTab'
   };
